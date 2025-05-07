3abb732c884c55e4a862fb8b2d403a17aa57d246
ErvinGU <ervin.garay+1@gmail.com>
2025-05-07 17:34:09 +0000

    ajustando js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,25 @@
 
 
 
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+//se aplica desplazamiento suave
+$(document).ready(function () {
+    $(".nav-link").on("click", function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $("html, body").animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+        }
+    });
+});
+
